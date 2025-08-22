7e83ce72c6d8260ae1b102157f60a5ec519595ce
Crouvizier Thomas <tcrouvizier@gmail.com>
2025-08-22 11:02:52 +0200

    add reaction rules

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMessageReactions,
     GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
   ],
 });
 
@@ -23,7 +25,7 @@ function saveBalances() {
   fs.writeFileSync(DATA_FILE, JSON.stringify(balances, null, 2));
 }
 
-// Nom de ta monnaie
+// Nom de la monnaie
 const CURRENCY = "🪙 Magik Coin";
 
 // Quand le bot est prêt
@@ -39,14 +41,14 @@ client.on("messageCreate", (message) => {
   const command = args.shift().toLowerCase();
 
   // Vérifier le solde
-  if (command === "!balance") {
+  if (command === "!solde") {
     const userId = message.author.id;
     const balance = balances[userId] || 0;
     message.reply(`Tu as ${balance} ${CURRENCY}`);
   }
 
   // Ajouter de l'argent (admin only)
-  if (command === "!addmoney") {
+  if (command === "!Acoin") {
     if (!message.member.permissions.has("Administrator")) {
       return message.reply("🚫 Tu n'as pas la permission.");
     }
@@ -55,7 +57,7 @@ client.on("messageCreate", (message) => {
     const amount = parseInt(args[1]);
 
     if (!mention || isNaN(amount)) {
-      return message.reply("Usage : `!addmoney @user 50`");
+      return message.reply("Usage : `!addmagikcoin @user 50`");
     }
 
     balances[mention.id] = (balances[mention.id] || 0) + amount;
@@ -63,8 +65,26 @@ client.on("messageCreate", (message) => {
     message.reply(`${amount} ${CURRENCY} ajoutés à ${mention.username}`);
   }
 
+  // Retirer de l'argent (admin only)
+  if (command === "!Rcoin") {
+    if (!message.member.permissions.has("Administrator")) {
+      return message.reply("🚫 Tu n'as pas la permission.");
+    }
+
+    const mention = message.mentions.users.first();
+    const amount = parseInt(args[1]);
+
+    if (!mention || isNaN(amount)) {
+      return message.reply("Usage : `!removemagikcoin @user 50`");
+    }
+
+    balances[mention.id] = (balances[mention.id] || 0) - amount;
+    saveBalances();
+    message.reply(`${amount} ${CURRENCY} retirés à ${mention.username}`);
+  }
+
   // Classement
-  if (command === "!leaderboard") {
+  if (command === "!classement") {
     let ranking = Object.entries(balances)
       .sort((a, b) => b[1] - a[1])
       .slice(0, 10);
@@ -73,7 +93,7 @@ client.on("messageCreate", (message) => {
       return message.reply("Personne n’a encore de monnaie !");
     }
 
-    let msg = "🏆 **Leaderboard** 🏆\n";
+    let msg = "🏆 **Classement** 🏆\n";
     ranking.forEach(([userId, balance], index) => {
       const user = message.guild.members.cache.get(userId);
       msg += `**${index + 1}.** ${
@@ -85,4 +105,25 @@ client.on("messageCreate", (message) => {
   }
 });
 
-client.login(process.env.TOKEN); // 🔑 Remplace par ton token Discord
+// rules
+// ID du message et des rôles
+const MESSAGE_ID = process.env.rulesMessage; // remplace par l'ID du message
+const ROLE_A = process.env.roleReincarnee; // rôle à donner
+const ROLE_B = process.env.roleGuildeux; // rôle à vérifier avant
+
+// Quand un utilisateur ajoute la réaction
+client.on("messageReactionAdd", async (reaction, user) => {
+  if (reaction.message.id !== MESSAGE_ID) return; // seulement ce message
+  if (user.bot) return; // ignore les bots
+
+  const guild = reaction.message.guild;
+  const member = await guild.members.fetch(user.id);
+
+  // Vérifie si l'utilisateur n'a pas déjà le rôle B
+  if (!member.roles.cache.has(ROLE_B)) {
+    await member.roles.add(ROLE_A);
+    console.log(`🎉 Ajout du rôle Ame réincarnée à ${user.username}`);
+  }
+});
+
+client.login(process.env.TOKEN); // token bot discord
