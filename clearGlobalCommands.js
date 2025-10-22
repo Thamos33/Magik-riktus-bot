bbdf7c12cf1bc7f626dedcab5c5a8433d58e816e
Crouvizier Thomas <tcrouvizier@gmail.com>
2025-10-22 17:13:08 +0200

    add clear command

---
+++ clearGlobalCommands.js
@@ -0,0 +1,24 @@
+// clearGlobalCommands.js
+import { REST, Routes } from "discord.js";
+import "dotenv/config";
+
+if (!process.env.TOKEN || !process.env.CLIENT_ID) {
+  console.error("❌ TOKEN ou CLIENT_ID manquant !");
+  process.exit(1);
+}
+
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+
+async function clearGlobal() {
+  try {
+    console.log("🧹 Suppression des commandes globales...");
+    await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), {
+      body: [],
+    });
+    console.log("✅ Commandes globales supprimées !");
+  } catch (err) {
+    console.error("❌ Erreur :", err);
+  }
+}
+
+clearGlobal();
